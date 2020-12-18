be2968353feba7c8913b56c0299f5707db2d7dcd
Naman Oli <namanoli111@gmail.com>
2020-12-18 08:46:29 +0530

    second

---
+++ src/screens/StartScreen.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+
+const StartScreen = ({ navigation }) => (
+  <Background>
+    <Logo />
+    <Header>ಅಂtara</Header>
+    <Paragraph>
+      Get your nearest shop in on tap.
+    </Paragraph>
+    <Button mode="contained" onPress={() => navigation.navigate('LoginScreen')}>
+      Login
+    </Button>
+    <Button
+      mode="outlined"
+      onPress={() => navigation.navigate('RegisterScreen')}
+    >
+      Sign Up
+    </Button>
+  </Background>
+)
+
+export default StartScreen
